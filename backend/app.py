9c1b404d33142710b1751e14105c1c527dc8f273
Dhruv Sharma <dhruv97sharma@Dhruvs-MacBook-Air.local>
2024-09-17 17:58:18 +0530

    Added debug flag in flask app run to enable auto reload for code changes inside the container

--- backend/app.py
+++ backend/app.py
@@ -14,4 +14,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    # Added debug = True to enable auto reload on backend changes
+    app.run(host='0.0.0.0', port=8080, debug=True)
